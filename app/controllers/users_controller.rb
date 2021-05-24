6e8e5fccb2ee89838017c3c00d3f41ca4ef3ed37
dmitryprog <decoy-06@mail.ru>
2021-05-24 10:40:30 +0500

    "downcase username before saving to db"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,34 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/' \
+          '71269686e0f757ddb4f73614f43ae445?s=100'
+      ),
+      User.new(id: 2, name: 'Misha', username: 'aristofun')
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
 
   def show
-    @hello = 'Привет бродяга!'
+    @user = User.new(
+      name: 'Vadim',
+      username: 'installero',
+      avatar_url: 'https://static.boredpanda.com/blog/wp-content/uploads/2017/11/My-most-popular-pic-since-I-started-dog-photography-5a0b38cbd5e1e__880.jpg',
+    )
+
+    @questions = [
+      Question.new(text: 'How u doin?', created_at: Date.parse('20.05.2021')),
+      Question.new(text: 'wazap?', created_at: Date.parse('20.05.2021'))
+    ]
+    @new_question = Question.new
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,42 +3,42 @@ require 'openssl'
 class User < ApplicationRecord
   ITERATIONS = 20000
   DIGEST = OpenSSL::Digest::SHA256.new
+  USERNAME_REGEXP = /\A[a-zA-Z0-9_]+\Z/
+
+  attr_accessor :password
 
   has_many :questions
 
-  validates :email, :username, presence: true
+  validates :email, :username, :password, presence: true
   validates :email, :username, uniqueness: true
-
-
-  attr_accessor :password
-  validates :password, presence: true
   validates :password, confirmation: true
-
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :username, format: { with: /\A[a-zA-Z0-9_]+\Z/ }
+  validates :username, format: { with: USERNAME_REGEXP }
 
+  before_validation :downcase_username
   before_save :encrypt_password
-  before_save :downcase_username
 
-  def encrypt_password
-    if self.password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-
-      self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
-      )
-    end
-  end
-
+  private
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
   end
+  
+  private
   def self.authenticate(email, password)
     user = find_by(email: email)
     if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
       user
     else
       nil
+    end
+  end
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(
+        OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
     end
   end
 
