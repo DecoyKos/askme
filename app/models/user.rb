8a6f85b73cc4ed684563b60cf206fd22d848950a
dmitryprog <decoy-06@mail.ru>
2021-05-24 10:41:13 +0500

    "styleguide user"

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
   end
-  
+
   private
   def self.authenticate(email, password)
     user = find_by(email: email)
