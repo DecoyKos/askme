f46357fe91e26be57caa2a8a55897ac418ff8b24
dmitryprog <decoy-06@mail.ru>
2021-05-24 11:39:58 +0500

    "new block sum of questions"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
       asset_path 'avatar.png'
     end
   end
+
+  def declination(number, enot, enota, enotov)
+    modulo = number % 10
+    return enotov if (11..14).include?(number % 100)
+    return enot if modulo == 1
+    return enota if (2..4).include?(modulo)
+    return enotov if (5..9).include?(modulo)
+    return enotov
+  end
 end
